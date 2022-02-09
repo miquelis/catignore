3068fdce538f7ba8fced0089d86aaaed265f3fef
Raphael Miquelis <raphael.miquelis@g.globo>
2022-02-08 22:38:00 -0300

    feat(install.sh): Modifying program installation location and adding colors in terminal

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+## Colors
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
 
-DIR_INSTALL_CATIGNORE=/opt/catignore
-DIR_SERIVCE_CATIGNORE=/etc/profile.d/catignore.sh
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+ 
+######################################################
+DIR_INSTALL_CATIGNORE=/usr/bin
 
 VERSION="v1.1.3"
 
@@ -9,53 +25,53 @@ HELP="Script to download catignore software in its latest version!
 
 Usage:
 
-  sudo bash install.sh [flags]
+  ${GREEN}sudo bash ${PURPLE}install.sh ${CYAN}[flags]${NC}
 
 Flags:
 
-  --help, -h          
+  ${CYAN}--help, -h ${NC}         
     Help for catignore
 
-  --version, -v       
+   ${CYAN}--version, -v ${NC}       
     Print just the version number.
 
-  --install, -i [Options] 
+   ${CYAN}--install, -i [Options] ${NC} 
     Install from catignore on distributions: Linux
 
-      --system, -s [required]
+       ${CYAN}--system, -s [required]${NC} 
         Inform the supported OS
           
-          --architecture, -a [required]
+           ${CYAN}--architecture, -a [required]${NC} 
             Enter the architecture supported by the OS
 
             Example:
-              sudo bash install.sh --install -s linux -a amd64
+              ${GREEN}sudo bash ${PURPLE}install.sh ${CYAN}--install -s ${NC}linux ${CYAN}-a ${NC}amd64
 
-  --download, -d [Options]
+   ${CYAN}--download, -d [Options]${NC} 
     Download catignore for the given operating system.
 
         The file is saved in the directory where the script is running.
 
-      --system, -s [required]
+       ${CYAN}--system, -s [required]${NC} 
       Inform the supported OS
         
-        --architecture, -a [required]
+         ${CYAN}--architecture, -a [required]${NC} 
           Enter the architecture supported by the OS
 
           Example:
-            bash install.sh --download -s windows -a amd64
+            ${GREEN}bash ${PURPLE}install.sh ${CYAN}--download -s ${NC}windows ${CYAN}-a ${NC}amd64
 
-  --list, -l
+   ${CYAN}--list, -l${NC} 
     List all OS compatible with catignore
 
     Example:
-      bash install.sh --list
+      ${GREEN}bash ${PURPLE}install.sh ${CYAN}--list ${NC} 
 
-  --uninstall         
+   ${CYAN}--uninstall  ${NC}        
     Uninstall catignore from linux 
 
     Example:
-      sudo bash install.sh --uninstall"
+      ${GREEN}sudo bash ${PURPLE}install.sh ${CYAN}--uninstall ${NC} "
 
 
 # argsCli - Get the information passed by parameter
@@ -63,7 +79,7 @@ Flags:
 # Example: $1 = --install, $2 = -s, $3 = linux, $4 = -a and $5 = amd64
 function argsCli() {
 
-  [ -z "$1" ] && echo -e "Offer any option, use -h for help" && exit 1
+  [ -z "$1" ] && echo -e "${CYAN}Offer any option, use -h for help${NC}" && exit 1
 
   case $1 in
     --help | -h) # Show program help
@@ -93,7 +109,7 @@ function argsCli() {
       exit 0
     ;;
     *) ## Erro unknown flags
-      echo -e "Erro: unknown ${1} flag! Type -h or --help to check the flags."
+      echo -e " ${RED} Erro: unknown ${1} flag! Type -h or --help to check the flags.${NC}"
       exit 2
   esac
 }
@@ -105,13 +121,13 @@ function argsInstallDownloaded() {
 
   if [ -z "$2" ] || [ -z "$3" ];
   then  
-    echo -e "Warning: Flag -s and OS name is required, use -h for help" 
+    echo -e " ${YELLOW} Warning: Flag -s and OS name is required, use -h for help${NC}" 
     exit 1
   fi
   
   if [ -z "$4" ] || [ -z "$5" ];
   then  
-    echo -e "Warning: Flag -a and architecture name is required, use -h for help" 
+    echo -e " ${YELLOW} Warning: Flag -a and architecture name is required, use -h for help${NC}" 
     exit 1
   fi
 
@@ -123,7 +139,7 @@ function argsInstallDownloaded() {
       exit 0
     ;;   
     *) # Erro unknown flags
-      echo -e "Erro: unknown ${1} flag! Use the -s flag and OS name to download."
+      echo -e " ${RED} Erro: unknown ${1} flag! Use the -s flag and OS name to download.${NC}"
       exit 2
   esac
 }
@@ -139,12 +155,12 @@ function argsCliArch() {
       local retval=$( checkArchiteture $2)
 
       if [[ $retval != "true" ]]; then
-        echo -e "Warning: Architecture $2 not found for download"  
+        echo -e " ${YELLOW} Warning: Architecture $2 not found for download${NC}"  
         exit 3
       fi          
     ;;
     *) # Erro unknown flags
-      echo -e "Erro: unknown ${1} flag! Use the -a flag and architecture name to download."
+      echo -e " ${RED} Erro: unknown ${1} flag! Use the -a flag and architecture name to download.${NC}"
       exit 3
   esac
 }
@@ -170,13 +186,13 @@ function checkSO() {
   "windows")  
    
     if [[ "$3" == "arm64" ]]; then
-      echo -e "Warning: There is no architecture version ${3} for ${2}"
+      echo -e " ${YELLOW} Warning: There is no architecture version ${3} for ${2}${NC}"
       exit 2 
     fi
    
     if [[ "$1" == "--install" ]] ||  [[ "$1" == "-i" ]];
     then     
-      echo -e "Warning: We still don't have catignore installation on OS ${2}."
+      echo -e " ${YELLOW} Warning: We still don't have catignore installation on OS ${2}.${NC}"
     elif [[ "$1" == "--download" ]] ||  [[ "$1" == "-d" ]];
     then
       donwloadCatIgnore $2 $3 
@@ -187,13 +203,13 @@ function checkSO() {
   "darwin")
 
     if [[ "$3" == "386" ]]; then
-      echo -e "Warning: There is no architecture version ${3} for darwin"
+      echo -e " ${YELLOW} Warning: There is no architecture version ${3} for darwin${NC}"
       exit 2 
     fi
    
     if [[ $1 == "--install" ]] ||  [[ $1 == "-i" ]];
     then     
-      echo -e "Warning: We still don't have catignore installation on OS ${2}."
+      echo -e " ${YELLOW} Warning: We still don't have catignore installation on OS ${2}.${NC}"
     elif [[ $1 == "--download" ]] ||  [[ $1 == "-d" ]];
     then
       donwloadCatIgnore $2 $3 
@@ -202,7 +218,7 @@ function checkSO() {
     exit 0
   ;;
   *) # Erro unknown flags
-    echo -e "Erro: unknown ${1} flag! Type -h or --help to check the flags."
+    echo -e " ${RED} Erro: unknown ${1} flag! Type -h or --help to check the flags.${NC}"
     exit 2
   esac
 
@@ -234,22 +250,17 @@ function installCatIgnoreLinux() {
 
   requireSudo
 
-  mkdir -p $DIR_INSTALL_CATIGNORE
+  # mkdir -p $DIR_INSTALL_CATIGNORE
   cd $DIR_INSTALL_CATIGNORE
 
   donwloadCatIgnore $1 $2
 
-  echo -e "Info: Installing catignore, please wait...."
+  echo -e "${GREEN} Info: Installing catignore, please wait....${NC}"
 
   tar xzf catignore-*
   rm -rf catignore-*
 
-  if ! grep -xq "catignore.sh" /etc/profile.d/*.sh &> /dev/null; then
-    echo -e "alias catignore=$DIR_INSTALL_CATIGNORE/catignore" > $DIR_SERIVCE_CATIGNORE  
-    source $DIR_SERIVCE_CATIGNORE
-  fi
-
-  echo -e "Info: Installation successfull! Close your terminal and open it again."
+  echo -e "${GREEN} Info: Installation successfull! Close your terminal and open it again.${NC}"
 
   exit 0 
 }
@@ -257,8 +268,8 @@ function installCatIgnoreLinux() {
 #requireSudo - Check if the script is running as sudo
 function requireSudo() {
   if [[ $UID != 0 ]]; then
-    echo -e "Warning: Please run this script with sudo:"
-    echo -e "sudo bash $0 $*"
+    echo -e " ${YELLOW} Warning: Please run this script with sudo:${NC}"
+    echo -e " ${GREEN}sudo bash ${PURPLE}$0 $* ${NC}"
     exit 1
   fi
 }
@@ -270,7 +281,7 @@ function donwloadCatIgnore() {
   local SO=$1
   local ARCHITECTURE=$2
 
-  echo "Info: Downloading the latest version of the program for the system ${SO}-${ARCHITECTURE}\n"
+  echo -e " ${GREEN}Info: Downloading the latest version of the program for the system ${SO}-${ARCHITECTURE}\n${NC}"
  
   local DOWNLOAD=$(wget -qO- https://api.github.com/repos/miquelis/catignore/releases/latest \
   | grep browser_download_url \
@@ -280,17 +291,17 @@ function donwloadCatIgnore() {
   wget -t 3 $DOWNLOAD
  
   if [  $? -ne 0 ]; then
-    echo -e "Erro: Failed to download the latest version of the program for the system ${SO}-${ARCHITECTURE}\n"
+    echo -e " ${RED} Erro: Failed to download the latest version of the program for the system ${SO}-${ARCHITECTURE}\n${NC}"
 
-    echo -e "Warning: Removing installation, please wait...\n"
+    echo -e " ${YELLOW} Warning: Removing installation, please wait...\n${NC}"
     uninstallLinux
     exit 1
   fi 
   
   local DOWNLOAD_NAME=$(echo $DOWNLOAD | cut -d"/" -f 9)
   
-  echo -e "Info: Download successfull!"
-  echo -e "Info: $PWD/${DOWNLOAD_NAME} has been saved successfully!"
+  echo -e "${GREEN} Info: Download successfull!${NC}"
+  echo -e "${GREEN} Info: $PWD/${DOWNLOAD_NAME} has been saved successfully!${NC}"
 }
 
 # uninstallLinux - Removes catignore
@@ -299,17 +310,16 @@ function uninstallLinux(){
 
   requireSudo
 
-  echo -e "Info: Uninstall catignore, please wait..."
+  echo -e "${GREEN} Info: Uninstall catignore, please wait...${NC}"
 
-  rm -rf $DIR_INSTALL_CATIGNORE
-  rm -rf $DIR_SERIVCE_CATIGNORE
+  rm -rf $DIR_INSTALL_CATIGNORE/catignore
   
   if [ $? -ne 0 ]; then
-    echo -e "Erro: Unistall catignore failed, try again later!"
+    echo -e " ${RED} Erro: Unistall catignore failed, try again later!${NC}"
     exit 1
   fi 
 
-  echo -e "Info: Uninstall successfull! Close your terminal and open it again."
+  echo -e "${GREEN} Info: Uninstall successfull! Close your terminal and open it again.${NC}"
 
   exit 0
 }
@@ -324,16 +334,16 @@ function listVersions() {
 
   
   if [ -z $LIST_DOWNLOAD ]; then
-    echo -e "Erro: Failed to list catignore versions, try again later!"
+    echo -e " ${RED} Erro: Failed to list catignore versions, try again later!${NC}"
     exit 1
   fi 
 
-  echo -e "\nList of latest versions:"
+  echo -e "\n ${CYAN} List of latest versions:${NC}\n"
 
   for value in "${LIST_DOWNLOAD[@]}"
   do
       local version=$(echo $value | cut -d"/" -f 9 | cut -d"." -f4,5 --complement)
-      echo -e " -> $version"
+      echo -e "-> $version"
   done 
 
   exit 0
